e4b5fb111a53ac83e0f2045480bfb8f3a176edac
diana m <dianamng@bu.edu>
2025-07-06 14:59:29 -0400

    built-in functions & arithmetic practice

--- firstScript.R
+++ firstScript.R
@@ -1,1 +1,10 @@
-print("Hello world!!")
+#### Arithmetic practice ####
+a = (3+4-5)*9 
+b = 99/33
+c = log(1)
+d = (sqrt(2))^2
+eVar = exp(3+4)
+f = abs(2^3 - 3^2)
+
+#### Built-in functions ####
+sum(1,1)
